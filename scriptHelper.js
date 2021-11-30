ea5b22e9a4e813935158ad382d7cbfd9a1e3d9d0
tah-dah <markcfranz13@gmail.com>
2021-11-30 10:04:48 -0800

    cargo mass too light added, copy paste from to heavy changed (fuelLevel < 10000 && cargoLevel <= 10000)

--- scriptHelper.js
+++ scriptHelper.js
@@ -47,6 +47,14 @@ if (cargoLevel > 10000 && fuelLevel < 10000){
     cargoStatus.innerHTML = "Cargo mass too heavy for launch";
 
 } else if (fuelLevel < 10000 && cargoLevel <= 10000)
+    list.style.visibility= "visible";
+    h2.style.color = "rgb(255, 0, 0)";
+    h2.innerHTML = "Shuttle not ready for launch";
+    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+    fuelStatus.innerHTML = "Fuel level too low for launch";
+    cargoStatus.innerHTML = "Cargo mass too light for launch";
+    
 async function myFetch() {
     let planetsReturned;
 
